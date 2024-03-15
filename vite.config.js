ffe0bfb801d4fb0a9a3948a7de734d0c27557206
Duch <duchvlado@gmail.com>
2024-03-15 08:00:45 +0100

    github page fix 2

--- vite.config.js
+++ vite.config.js
@@ -7,11 +7,6 @@ import vue from "@vitejs/plugin-vue";
 export default defineConfig({
   plugins: [vue()],
   base: "./",
-  resolve: {
-    alias: {
-      "@": fileURLToPath(new URL("./src", import.meta.url)),
-    },
-  },
   optimizeDeps: {
     include: ['jscharting'],
 },
